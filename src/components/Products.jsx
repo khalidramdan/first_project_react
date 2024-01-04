73a43b48a6bc386151b43acd5313a7558118ba2a
khalid ramadan <ramdan.khalid1234@gmail.com>
2024-01-04 14:23:19 +0100

    delete_btn_and_check_btn

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,5 @@
 import { faCheckCircle, faCheckSquare, faCircle } from '@fortawesome/free-regular-svg-icons'
-import { faRadiation } from '@fortawesome/free-solid-svg-icons'
+import { faPen, faRadiation, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useState } from 'react'
 
@@ -10,6 +10,19 @@ function Products() {
         { id: 3, name: "Smart Phone", price: 3000, checked: true },
         { id: 4, name: "Scan", price: 2300, checked: false },
     ])
+    const handleDeleteProduct = (product) =>{
+        const newProducts = products.filter((p)=>p.id != product.id);
+       setProducts(newProducts);
+    }
+    const handleCheckedProduct = (product)=>{
+        const newProducts = products.map(p=>{
+            if(p.id == product.id) {
+                 p.checked = !p.checked
+        }
+        return p;
+        });
+        setProducts(newProducts);
+    }
     return (
         <div className="p-3">
             <div className="row">
@@ -23,6 +36,7 @@ function Products() {
                                         <th>Name</th>
                                         <th>Price</th>
                                         <th>Checked</th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -34,12 +48,20 @@ function Products() {
                                                 <td>{product.price}</td>
                                                 <td>
                                                 {
-                                                    product.checked == true ? <button className='btn btn-outline-success'>
+                                                    product.checked == true ? <button className='btn btn-outline-success' onClick={()=> handleCheckedProduct(product)}>
                                                     <FontAwesomeIcon icon={faCheckCircle} ></FontAwesomeIcon>
-                                                </button> : <button className='btn btn-outline-danger'>
+                                                </button> : <button className='btn btn-outline-danger' onClick={()=> handleCheckedProduct(product)}>
                                                 <FontAwesomeIcon icon={faCircle} ></FontAwesomeIcon>
                                                 </button> 
                                                 }   
+                                                </td>
+                                                <td> 
+                                                    <button onClick={()=>handleDeleteProduct(product)} className='btn btn-outline-danger'>
+                                                        <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                                                    </button>
+                                                    <button className='btn btn-outline-success m-1'>
+                                                        <FontAwesomeIcon icon={faPen}></FontAwesomeIcon>
+                                                    </button>
                                                 </td>
                                             </tr>
                                         ))
